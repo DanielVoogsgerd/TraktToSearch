eb2323aeb3f9f8297000c30dead4babf2bc67959
Daniel Voogsgerd <daniel@voogsgerd.nl>
2015-09-27 16:22:13 +0200

    Why use onclick on a link when you can just use the href

--- magic.js
+++ magic.js
@@ -1,16 +1,10 @@
-function test() {
-    var name = document.querySelector('meta[property="og:title"]').content;
-
-    window.location = "http://search.student.utwente.nl/search?q=" + name;
-}
-
-
 if(document.querySelector('meta[property="og:type"]').content == "video.movie") {
 
     var actionButtonDiv = document.querySelector('.action-buttons');
+    var name = document.querySelector('meta[property="og:title"]').content;
 
     var buttonCode =
-    '<a id="findOnSearchButton" class="btn btn-block btn-summary btn-checkin" data-movie-id="102924" data-type="movie" data-url="/movies/tomorrowland-2015" href="#">' +
+    '<a id="findOnSearchButton" class="btn btn-block btn-summary btn-checkin" data-movie-id="102924" data-type="movie" data-url="/movies/tomorrowland-2015" href="http://search.student.utwente.nl/search?q=' + name + '">' +
         '<div class="fa fa-fw trakt-icon-list">' +
         '</div>' +
         '<div class="text">' +
@@ -26,8 +20,5 @@ if(document.querySelector('meta[property="og:type"]').content == "video.movie") 
         '</div>'+
     '</a>';
 
-
     actionButtonDiv.innerHTML = actionButtonDiv.innerHTML + buttonCode;
-
-    document.getElementById('findOnSearchButton').addEventListener('click', test);
 }
